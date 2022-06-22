205526f244d90a73241ce7cd9c93c18dafb8d95e
edwin666666 <houlijun@petalmail.com>
2022-06-22 09:55:53 +0800

    master

--- chapter3-数据类型/demo7-字符类型基本使用细节/main.go
+++ chapter3-数据类型/demo7-字符类型基本使用细节/main.go
@@ -12,5 +12,8 @@ func main() {
 	//如果我们希望输出字符，需要使用格式化输出
 	fmt.Printf("a=%c \nb=%c", a, b)
 	var c int = '北'
-	fmt.Printf("\nc=%c", c)
+	fmt.Printf("\nc=%c\n", c)
+	//字符类型是可以进行运算，相当于一个证书，运算时是按照码值运行
+	var d = 10 + 'a' // 10+97=107
+	fmt.Println("d=", d)
 }
